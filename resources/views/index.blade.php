e922c683ef57c584baff5539d3a48581523d518c
Mateus Pinheiro <teusbarros@Mateuss-MacBook-Pro.local>
2023-01-11 12:56:17 -0300

    First: laravel 8, vue 2.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Kanban</title>
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+        <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+    </body>
+</html>
